37f88878b96df81ae6415bfad9fcdf316fccb4bf
Anne Rayssa Mandú <103398998+rayssamandu@users.noreply.github.com>
2022-05-15 12:13:53 -0300

    Primeira atualização do projeto

--- projeto-forca.py
+++ projeto-forca.py
@@ -1,1 +1,72 @@
-##Projeto Forca
+palavra = input("Digite a palavra secreta:").lower().strip()
+for x in range(100):
+    print()
+digitadas = []
+acertos = []
+erros = 0
+while True:
+    senha = ""
+    for letra in palavra:
+        senha += letra if letra in acertos else "."
+    print(senha)
+    if senha == palavra:
+        print("Parabéns, você ganhou!")
+        print("       ___________      ")
+        print("      '._==_==_=_.'     ")
+        print("      .-\\:      /-.    ")
+        print("     | (|:.     |) |    ")
+        print("      '-|:.     |-'     ")
+        print("        \\::.    /      ")
+        print("         '::. .'        ")
+        print("           ) (          ")
+        print("         _.' '._        ")
+        print("        '-------'       ")
+        break
+    tentativa = input("\nDigite uma letra:").lower().strip()
+    if tentativa in digitadas:
+        print("Você já tentou esta letra!")
+        continue
+    else:
+        digitadas += tentativa
+        if tentativa in palavra:
+            acertos += tentativa
+        else:
+            erros += 1
+            print("Você errou!")
+    print("X==:==\nX  :   ")
+    print("X  O   " if erros >= 1 else "X")
+    linha2 = ""
+    if erros == 2:
+        linha2 = "  |   "
+    elif erros == 3:
+        linha2 = " \|   "
+    elif erros >= 4:
+        linha2 = " \|/ "
+    print("X%s" % linha2)
+    linha3 = ""
+    if erros == 5:
+        linha3 += " /     "
+    elif erros >= 6:
+        linha3 += " / \ "
+    print("X%s" % linha3)
+    print("X\n===========")
+    if erros == 6:
+        print("Puxa, você foi enforcado!")
+        print("A palavra era {}".format(palavra))
+        print("    _______________         ")
+        print("   /               \       ")
+        print("  /                 \      ")
+        print("//                   \/\  ")
+        print("\|   XXXX     XXXX   | /   ")
+        print(" |   XXXX     XXXX   |/     ")
+        print(" |   XXX       XXX   |      ")
+        print(" |                   |      ")
+        print(" \__      XXX      __/     ")
+        print("   |\     XXX     /|       ")
+        print("   | |           | |        ")
+        print("   | I I I I I I I |        ")
+        print("   |  I I I I I I  |        ")
+        print("   \_             _/       ")
+        print("     \_         _/         ")
+        print("       \_______/           ")
+        break
